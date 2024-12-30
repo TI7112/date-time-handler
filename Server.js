27096c2c98e68fb08d4ead67e9670d94c4b2b49d
TI7112 <ti7112@outlook.com>
2024-12-30 23:27:19 +0530

    Initial Deploy

---
+++ Server.js
@@ -0,0 +1,22 @@
+const days = ["sunday", "monday", "tuesday", "wednesday", "thrusday", "friday", "saturday"]
+const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+function now() {
+    const date = new Date();
+    return date;
+}
+
+function detail(bufferDate) {
+    const date = new Date(bufferDate);
+    const data = {
+        day: date.getDay(),
+        dayName: days[date.getDay()],
+        date: date.getDate(),
+        month: date.getMonth(),
+        monthName: months[date.getMonth()],
+        noOfDays: new Date(date.getFullYear(), date.getMonth()+1, 0).getDate(),
+        year: date.getFullYear(),
+    }
+}
+
+detail("2024-02-21")
